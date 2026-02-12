e4725a205d652deb095135c404c999f601413269
Shreyansh Das <dasshreyansh04@gmail.com>
2026-02-12 21:08:42 +0530

    First Commit

---
+++ src/test/java/com/campusEvent/campus_event/CampusEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campusEvent.campus_event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
